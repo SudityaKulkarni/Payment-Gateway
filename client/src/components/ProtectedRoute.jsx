06f91b5000ebdfd13f8d32a1ea282c361ef4b5de
SudityaKulkarni <suditya.kulkarni@gmail.com>
2026-02-26 23:02:24 +0530

    finally got it to work

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 export default function ProtectedRoute({ children }) {
   const isAuthenticated = localStorage.getItem("token");
 
-  if (!isAuth) {
+  if (!isAuthenticated) {
     return <Navigate to="/signin" />;
   }
 
--- client/src/pages/Profile.jsx
+++
@@ -1,9 +0,0 @@
-export default function Profile() {
-  return (
-    <div className="min-h-screen bg-gray-950 text-white flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-gray-500">
-        Profile Page (Under Development)
-      </h1>
-    </div>
-  );
-}
